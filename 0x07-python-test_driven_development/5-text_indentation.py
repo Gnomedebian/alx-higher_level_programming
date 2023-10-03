896bea1822ffa6162f4f6f87c8748f2a9dd6842a
Gnomedebian <yassineamgarou@gmail.com>
2023-10-03 23:32:54 +0100

    edit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""
+This module prints something
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
+    prints a text with 2 new lines
+
+    Parameters:
+        text: text to print
+
+    Raises:
+        TypeError: text must be a string
+    """
+    #text must be a string
+    if type(text) is not str:
         raise TypeError("text must be a string")
+    
+    #print
+    for i in range(len(text)):
+        if text[i] == "?":
+            print("\n")
+        else:
+            print(text[i], end="")
+"""
+try:
+    print_square(-5)
+except Exception as e:
+    print(e)
+"""
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+text_indentation('test test test ? max max')
