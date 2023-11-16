9da27c794d23383125198bcb408489f37c01de53
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2023-11-16 01:09:31 +0100

    edit and push

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create datbase & user: select only privlage
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'; 
