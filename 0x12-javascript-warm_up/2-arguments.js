452d4c859ce9de6a8722177711e87ab728603671
Gnomedebian <yassineamgarou@gmail.com>
2023-12-11 18:10:33 +0100

    push task_2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv === 0) {
+const args = process.argv.length;
+if (args === 2) {
   console.log('No argument');
-} else if (argv === 1) {
+} else if (args === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
