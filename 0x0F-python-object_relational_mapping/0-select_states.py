f2f1e78d91f88f185161290256d54f34b24f9694
Gnomedebian <yassineamgarou@gmail.com>
2023-12-17 15:49:15 +0100

    push

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+'''lists all states from the database'''
 import MySQLdb
+
 import sys
 
-db = MySQLdb.connect(host='localhost',
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3],
-                     port=3306)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM hbtn_0e_0_usa"
-               "ORDER BY states.id ASC")
-rows = cursor.fetchall()
-for row in rows:
-    for col in row:
-        print(col)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM hbtn_0e_0_usa"
+                "ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
