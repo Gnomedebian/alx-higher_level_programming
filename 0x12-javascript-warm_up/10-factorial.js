d68d1559804d7764d3d4e5563f86fbc12c3166a3
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2024-01-08 22:32:22 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-function fact (i) {
-  return i === 0 || isNaN(i) ? 1 : i * fact(i - 1);
-}
-
-console.log(fact(Number(process.argv[2])));
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const sentence = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < sentence.length; i++) {
-  console.log(`${sentence[i]}`);
-}
