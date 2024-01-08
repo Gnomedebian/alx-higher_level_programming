e1c75a2aa50f388cf2ef8a1575efc2998ddc71da
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2024-01-08 22:32:34 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-module.exports = class Square extends require('./5-square') {
-  charPrint (a) {
-    if (a === undefined) { this.print(); } else {
-      for (let i = 0; i < this.height; i++) { console.log(a.repeat(this.width)); }
-    }
-  }
-};
