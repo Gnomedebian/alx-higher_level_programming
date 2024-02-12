a4b3d60089806354ebd46795e54ab96108f80718
Gnomedebian <yassineamgarou@gmail.com>
2024-02-12 21:13:06 +0100

    push task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+const args = process.argv;
+if (args.length < 3) {
+  console.log(`${undefined} is ${undefined}`);
+} else if (args.length === 3) {
+  console.log(`${args[2]} is ${undefined}`);
+} else {
+  console.log(`${args[2]} is ${args[3]}`);
+}
