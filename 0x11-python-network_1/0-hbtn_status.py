2251bdbd5190a9d7125ee72e4c7721f58115eb52
Gnomedebian <yassineamgarou@gmail.com>
2024-03-02 00:39:05 +0100

    py-network_1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''task 0: fetch data'''
+
+if __name__ == "__main__":
+    import urllib.request
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as r:
+        response = r.read()
+        print('Body response:')
+        print(f'\t- type: {type(response)}')
+        print(f'\t- content: {response}')
+        print(f'\t- utf8 content: {response.decode("utf-8")}')
