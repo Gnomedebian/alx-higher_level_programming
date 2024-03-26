861c39428033bcbbfdb4e653f13e05860f53ee36
Gnomedebian <yassineamgarou@gmail.com>
2024-03-26 15:57:23 +0000

    task::writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const filePath = 'process.argv[2]';
 const content = 'Python is cool';
 
-fs.writeFile(filePath, content, (err) => {
+fs.writeFile(filePath, 'utf8', content, (err) => {
   if (err) {
     console.error(err);
   }
