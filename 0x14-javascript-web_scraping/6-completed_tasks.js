f2af3c90bf661f5a71add74c2d37cf14ceef2302
Gnomedebian <yassineamgarou@gmail.com>
2024-03-27 00:20:25 +0000

    task::js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
+const apiUrl = process.argv[2];
 
-request.get(url, function (error, response, body) {
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    const endPoint = {};
-    for (const x of data) {
-      if (x.endPoint === true) {
-        if (x.userId in endPoint) {
-          endPoint[x.userId]++;
-        } else {
-          endPoint[x.userId] = 1;
-        }
+    console.error('Error:', error);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
       }
     }
-    console.log(endPoint);
-  }
+  });
+  console.log(completedTasks);
 });
