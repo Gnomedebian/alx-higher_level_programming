7d5a89c2c11a296dac801a0f5ff64e4b390b1638
Gnomedebian <yassineamgarou@gmail.com>
2024-03-27 00:09:10 +0000

    task::js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    for (const film of films) {
+      const characters = film.characters;
+      for (const characterUrl of characters) {
+        const id = characterUrl.split('/').slice(-2, -1)[0];
+        if (id === characterId) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
